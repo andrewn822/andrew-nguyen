98ce6a9b4fd3b885c7c758aa1eda4b37ba79a507
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Pro.local>
2022-11-07 13:49:50 -0800

    cat changes

--- index.js
+++ index.js
@@ -3,5 +3,14 @@ var image = document.getElementById('different-cat');
 
 
 image.addEventListener('click', function(image){
-  image.target.classList.toggle('greencat');
+  var catType = image.target;
+  console.log(catType);
+
+  if (this.classList == 'cat-container') {
+    image.target.classList.toggle('greencat');
+  } else {
+    image.target.classList.toggle('yellowcat');
+  } 
+
 })
+
