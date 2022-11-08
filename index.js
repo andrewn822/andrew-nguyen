963614037d02ad4d8f0d95db56bfc69da5313ed4
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Pro.local>
2022-11-07 18:15:45 -0800

    cat changes

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ var image = document.getElementById('different-cat');
 
 
 image.addEventListener('click', function(image){
+  image.target.classList.toggle('greencat');
   var catType = image.target;
   console.log(catType);
 
-  if (this.classList == 'cat-container') {
-    image.target.classList.toggle('greencat');
-  } else {
-    image.target.classList.toggle('yellowcat');
-  } 
+  // if (this.classList == 'cat-container') {
+  //   image.target.classList.toggle('greencat');
+  // } else {
+  //   image.target.classList.toggle('purplecat');
+  // } 
 
 })
 
